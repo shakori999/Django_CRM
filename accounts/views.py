cbb4a91e78f7b8d0dc60f311ab843bf82cc7e1cb
shakori999 <shakori999@gmail.com>
2021-10-26 21:50:38 +0300

    correctd the structure , made the URLS and Views

---
+++ accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("home page")
+
+def products(request):
+    return HttpResponse("product page")
+
+def customer(request):
+    return HttpResponse("user page")
