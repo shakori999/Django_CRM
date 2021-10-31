42b38eade8e46c410d384fae5586ea4182825abe
shakori999 <shakori999@gmail.com>
2021-10-31 07:49:26 +0300

    add relationships to orders/customers

--- accounts/models.py
+++ accounts/models.py
@@ -10,6 +10,12 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     CATEGORY = (
         ('Indoor','Indoor'),
@@ -18,8 +24,12 @@ class Product(models.Model):
     name = models.CharField(max_length=20, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=20, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=20, null=True)
+    description = models.CharField(max_length=20, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
@@ -28,7 +38,7 @@ class Order(models.Model):
         ('Out For Delivery','Out for delivery'),
         ('Delivered','Deliverd'),
     )
-    # customer = 
-    # product = 
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=20, null=True, choices=STATUS)
